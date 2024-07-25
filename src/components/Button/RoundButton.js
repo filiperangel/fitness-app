6848a8d8a8112a8747304d7b257f2416863e4652
Filipe Rangel <filiperangel@gmail.com>
2024-07-25 07:35:48 +0100

    - adding sizes constant file

--- src/components/Button/RoundButton.js
+++ src/components/Button/RoundButton.js
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import { Button } from "@mui/material";
+import { DEFAULT_BUTTON_HEIGHT } from "../../contants/sizes";
 
 const RoundButtonStyled = styled(Button)(({ theme, color }) => {
   return `
   background: linear-gradient(45deg, ${theme.palette[color].alternative}, ${theme.palette[color].main});
   border-radius: 25px;
-  height: 55px;
+  height: ${DEFAULT_BUTTON_HEIGHT};
   font-size: 17px;
 `;
 });
